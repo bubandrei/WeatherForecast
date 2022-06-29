e59e3e3ba860dfd7b05a0ac7a48ea75a6ba7c688
a.bubelev <bubeleu@gmail.com>
2022-06-29 13:05:47 +0300

    api

--- my-app/src/component/Weather.jsx
+++ my-app/src/component/Weather.jsx
@@ -1,15 +1,24 @@
 import { useEffect, useState } from "react";
 
-const Weather = ({ city }) => {
-  const [current, setCurrent] = useState();
-  useEffect(() => {
+const Weather = ({ city }) =>
+{
+  const [ current, setCurrent ] = useState(null);
+  useEffect(() =>
+  {
     const url =
-      "https://api.openweathermap.org/data/2.5/weather?q="+ city + "&units=metric&appid=672cb8d270d19ebf2dbdf49d3b1234a5";
+      // "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=672cb8d270d19ebf2dbdf49d3b1234a5";
     fetch(url)
       .then((res) => res.json())
-      .then((json) => setCurrent(json.main.temp));
-  }, [city]);
-  console.log(current);
-  return <h1>{city} - {current}</h1>;
+      .then((data) => console.log(data));
+  }, [ city ]);
+  console.log(current)
+  return (
+    <>
+      <h1>{ city }</h1>
+      <div>{ current }</div>
+      <div>{current.weather[0].main}</div>
+      <div>{current.wind.speed}</div>
+    </>
+  )
 };
 export default Weather;
